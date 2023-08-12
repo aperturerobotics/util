0948cebca3a2b49c71d4630fd1e8a2eae2ed01df
Christian Stewart <christian@aperture.us>
2023-08-11 19:40:47 -0700

    broadcast: add lock function
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- broadcast/broadcast.go
+++ broadcast/broadcast.go
@@ -16,19 +16,36 @@ type Broadcast struct {
 // GetWaitCh returns a channel that is closed when Broadcast is called.
 func (c *Broadcast) GetWaitCh() <-chan struct{} {
 	c.mtx.Lock()
-	if c.ch == nil {
-		c.ch = make(chan struct{})
-	}
-	ch := c.ch
-	c.mtx.Unlock()
-	return ch
+	defer c.mtx.Unlock()
+	return c.getWaitChLocked()
 }
 
+// Broadcast closes the broadcast channel, triggering waiters.
 func (c *Broadcast) Broadcast() {
 	c.mtx.Lock()
+	defer c.mtx.Unlock()
+	c.broadcastLocked()
+}
+
+// AccessLock locks the mutex and calls the callback.
+func (c *Broadcast) HoldLock(cb func(broadcast func(), getWaitCh func() <-chan struct{})) {
+	c.mtx.Lock()
+	defer c.mtx.Unlock()
+	cb(c.broadcastLocked, c.getWaitChLocked)
+}
+
+// broadcastLocked is the implementation of Broadcast while mtx is locked.
+func (c *Broadcast) broadcastLocked() {
 	if c.ch != nil {
 		close(c.ch)
 		c.ch = nil
 	}
-	c.mtx.Unlock()
+}
+
+// getWaitChLocked is the implementation of GetWaitCh while mtx is locked.
+func (c *Broadcast) getWaitChLocked() <-chan struct{} {
+	if c.ch == nil {
+		c.ch = make(chan struct{})
+	}
+	return c.ch
 }
